37f0c9ea28d8e6cfef34fbc26e0775db90420666
JoseMan72 <josemanuelbonillagarcia@gmail.com>
2023-11-16 10:58:53 +0100

    Primer commit

---
+++ Book/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+   title = models.CharField(max_length=50)
+   author = models.CharField(max_length=50)
+   rating = models.IntegerField()
+
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+      return self.title
