9339244354f21a5aa2c97d77f6c119b4b22e4d76
JoseMan72 <josemanuelbonillagarcia@gmail.com>
2023-11-16 12:17:10 +0100

    validator para rating en el modelo

--- Book/models.py
+++ Book/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Book(models.Model):
    title = models.CharField(max_length=50)
    author = models.CharField(max_length=50)
-   rating = models.IntegerField()
+   # maximo hasta el 5 en rating
+   rating = models.IntegerField(validators=[MaxValueValidator(5)])
    sinopsis = models.TextField()
 
-   created_at = models.DateTimeField(auto_now_add=True)
-   updated_at = models.DateTimeField(auto_now=True)
+   created_at = models.DateTimeField(auto_now_add=True) # auto_now_add=True: Cuando se crea el registro, se guarda la fecha y hora
+   updated_at = models.DateTimeField(auto_now=True) # auto_now=True: Cuando se actualiza el registro, se guarda la fecha y hora
 
    def __str__(self):
       return self.title
